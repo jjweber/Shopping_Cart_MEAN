ee76c100714bcc203836bf02f887b5d1898a2470
Justin Weber <jjweber@fullsail.edu>
2018-03-13 17:27:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -91,8 +91,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', api);
-
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
